65cc36d820eaec23537ccbfc9146f3c988630fc9
RinaHicksney <therapy3@mail.ru>
2019-09-16 07:24:38 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
@@ -2,7 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class Test {
 	public static void main (String[] args){
-System.out.println("Hello, world!");
-}
+        hello("world");
+        hello("Marina");
 
+        double l =5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 5;
+        double b = 7;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = "+ area(a,b));
+}
+    public static void hello (String some){
+        System.out.println("Hello, " + some + "!");
+    }
+
+    public static double area(double a){
+	    return a*a;
+    }
+
+    public static double area(double a, double b){
+	    return a*b;
+    }
 }
