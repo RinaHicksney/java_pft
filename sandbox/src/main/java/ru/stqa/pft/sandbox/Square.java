140d20f04691ecaa5afa518e8092482a0e1e2a4a
RinaHicksney <therapy3@mail.ru>
2019-09-17 07:29:48 +0300

    добавлены конструкторы в классы квадрат и прямоугольник

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -2,4 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Square {
     public double l;
+
+    public Square(double len){
+        this.l= len;
+    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
@@ -5,13 +5,10 @@ public class Test {
         hello("world");
         hello("Marina");
 
-        Square s = new Square();
-        s.l =5;
+        Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-        Rectangle r = new Rectangle();
-        r.a = 5;
-        r.b = 7;
+        Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = "+ area(r));
 }
     public static void hello (String some){
