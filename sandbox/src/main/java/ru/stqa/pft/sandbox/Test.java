0777a9b63b0c3f1f9e88725ad7c0d8c6ea50c2e1
RinaHicksney <therapy3@mail.ru>
2019-09-25 21:13:43 +0300

    добавлен метод в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
@@ -10,7 +10,7 @@ public class Test {
 
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = "+ r.area());
-
+/*
 	    Point p1 = new Point();
 	    p1.x = 3;
 	    p1.y = 6;
@@ -19,17 +19,22 @@ public class Test {
         p2.x = 5;
         p2.y = 4;
         System.out.println("Расстояние между двумя точками ="+ distance(p1,p2));
+*/
+        Point p1 = new Point(3, 6);
+        Point p2 = new Point(5, 4);
+        System.out.println("Расстояние между двумя точками ="+ p2.distance(3,6));
 
 	}
 
     public static void hello (String some){
         System.out.println("Hello, " + some + "!");
     }
+    /*
         //функция вычисления расстояния между точками
         public static double distance(Point p1, Point p2){
             return Math.sqrt((p2.x-p1.x)*((p2.x-p1.x))+(p2.y-p1.y)*(p2.y-p1.y));
         }
-
+*/
 
 
 }
