595e6f7e0bd60d28f512be5f9cf26c455d086bd8
RinaHicksney <therapy3@mail.ru>
2019-10-03 07:46:51 +0300

    добавлен класс SquareTests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+    public void testArea(){
+
+        @Test
+        Square s = new Square(5);
+        // assert s.area() == 25; простой ассерт, без вывода значения
+        Assert.assertEquals(s.area() , 25.0); //ассерт с выводом ожидаемого и факт. результата
+    }
+}
