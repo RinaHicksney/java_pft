3286adeed3033c17900affcd8ea59378238a34a1
Двинских Марина Юрьевна <dvinskikh@kontur>
2019-11-13 14:43:48 +0300

    добавлен класс ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContasctCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContasctCreationTests.java
@@ -8,8 +8,8 @@ public class ContasctCreationTests extends TestBase{
   @Test
   public void testContasctCreation() throws Exception {
     app.goToContactPage();
-    app.fillContactForm(new ContactData("First_Name", "Middle_Name", "Last_Name", "Nick_Name", "Title", "Company", "Address", "HOme", "MObile", "Work", "Fax", "test@mail.ru", "test2@mail.ru", "test3@mail.ru", "HOmepage"));
-    app.submitContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("First_Name", "Middle_Name", "Last_Name", "Nick_Name", "Title", "Company", "Address", "HOme", "MObile", "Work", "Fax", "test@mail.ru", "test2@mail.ru", "test3@mail.ru", "HOmepage"));
+    app.getContactHelper().submitContactCreation();
     app.returnToHomePage();
     app.logout();
   }
