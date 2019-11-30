c7fbe7609de7038212439f7383bd5f74d3b408e8
Двинских Марина Юрьевна <dvinskikh@kontur>
2019-11-30 15:36:55 +0300

    4.9 компоратор

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,14 +26,23 @@ public class GroupCreationTests extends TestBase{
     //Assert.assertEquals(after, before + 1); //количество ДО должно совпадать с ПОСЛЕ +1
     Assert.assertEquals(after.size(), before.size() + 1); //размер после = размер до +1
 
-
+    //способ сравнения 1
     int max = 0;
     for (GroupData g : after) {
       if (g.getId() > max) {
         max = g.getId();
       }
     }
-    group.setId(max);
+    //способ сравнения 2
+    //является анонимным классом
+    Comparator<? super GroupData> byId = new Comparator<GroupData>() {   //new+ctrl+space (создание переопределения)
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    }; //компаратор (сравниватель).  который умеет сравнивать объекты типа GroupData
+    int max1 = after.stream().max(byId).get().getId();
+    group.setId(max1);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
